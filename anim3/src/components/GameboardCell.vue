2efce3ff85cdfb76c5352ed009f5d05d5ab43d5f
Les Matheson <les.matheson@gmail.com>
2021-12-11 18:14:12 -0500

    Milestone: Button per cell, full-size of div, click emits button location in console

--- anim3/src/components/GameboardCell.vue
+++ anim3/src/components/GameboardCell.vue
@@ -1,44 +1,39 @@
 <template>
-  <div class="gameboard_cell" v-on:click="select_div($event,row,col)">
-      <button type="button" v-on:click="undefined">
-        row: {{row}}, col: {{col}}
-      </button>
-  </div>
+    <button
+        class="gameboard_button"
+        type="button"
+        v-on:click="select($event, row, col)"
+    >
+        row: {{ row }}, col: {{ col }}
+    </button>
 </template>
 
 <script>
 export default {
-    //name: 'GameboardCell',
     props: {
         row: Number,
-        col: Number
+        col: Number,
     },
     data() {
         return {
-            state: 'none'
-        }
+            state: "none",
+        };
     },
     methods: {
-        select: function(event,row,col) {
+        select: function (event, row, col) {
             // https://stackoverflow.com/a/36968187/237059  On event hookups
 
             const targetId = event?.currentTarget.id;
             console.log(`btn.targetId=${targetId},row=${row},col=${col}`);
-        },
-        select_div: function(event,row,col) {
-            // https://stackoverflow.com/a/36968187/237059  On event hookups
-
-            const targetId = event?.currentTarget.id;
-            console.log(`div.targetId=${targetId},row=${row},col=${col}`);
         }
-    }
-}
+    },
+};
 </script>
 
 <style>
-.gameboard_cell button {
-    width:100%;
-    height:100%;
-    display:block;
+.gameboard_button {
+    width: 100%;
+    height: 100%;
+    display: block;
 }
 </style>
